b65275f8c8abe6e5aa3aea65b6eebfe898da7ff6
plenglin <maximupwnguin@gmail.com>
2017-10-24 11:10:53 -0700

    Move in my original environment

---
+++ Assets/Scripts/SolarControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarControl : MonoBehaviour { 
+
+    public Vector3 offset;
+
+    public GameObject track;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion rotation = track.transform.rotation;
+        rotation *= Quaternion.Euler(offset);
+        this.transform.rotation = rotation;
+    }
+
+}
