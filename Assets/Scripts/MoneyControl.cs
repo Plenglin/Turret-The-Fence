e362b4c274b984bc27f5acdb440df46b110c1947
plenglin <maximupwnguin@gmail.com>
2017-11-02 12:07:10 -0700

    Add money control

---
+++ Assets/Scripts/MoneyControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyControl : MonoBehaviour {
+
+    public Text display;
+    public int money;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        display.text = string.Format("Money; ${0}", money);
+	}
+
+}
