31aed248063ee00c57331a2f5cef869d214c33f3
plenglin <maximumpwnguin@gmail.com>
2017-11-04 19:08:26 -0700

    This code doesn't work yet
    At least I don't think it does

--- Assets/Scripts/Enemy/SpawnManager.cs
+++ Assets/Scripts/Enemy/SpawnManager.cs
@@ -30,7 +30,11 @@ namespace SpawnManager {
     public class EnemySpawnManager : MonoBehaviour {
 
         public Transform[] spawnAreas;
-        public WaveDescription current;
+        public WaveDescription wave;
+
+        public int completedCoroutines;
+
+        private const float waitOnCoroutines = 0.25f;
 
         // Use this for initialization
         void Start() {
@@ -43,14 +47,40 @@ namespace SpawnManager {
         }
 
         IEnumerator DoWave() {
+            foreach (SemiWaveDescription sw in wave.spawns) {
+                switch (sw.type) {
+                    case BranchType.Sequential:
+                        foreach (SpawnDescription sp in sw.toSpawn) {
+                            yield return sp.DoSpawnAt(getRandomSpawnArea());
+                        }
+                        break;
+                    case BranchType.SimultaneousAll:
+                        completedCoroutines = 0;
+                        List<Coroutine> running = new List<Coroutine>();
+                        foreach (SpawnDescription sp in sw.toSpawn) {
+                            running.Add(StartCoroutine(sp.DoSpawnAt(getRandomSpawnArea())));
+                        }
+                        while (completedCoroutines < running.Count) {
+                            yield return new WaitForSeconds(waitOnCoroutines);
+                        }
+                        break;
+                }
+            }
             yield return new WaitForSeconds(10);
+        }
+
+        public Vector3 getRandomSpawnArea() {
+            return spawnAreas[Random.Range(0, spawnAreas.Length)].position;
+        }
+
+        public void OnCoroutineComplete() {
+            completedCoroutines++;
         }
 
     }
 
     public enum BranchType {
-        SimultaneousAll,  // Every SpawnDescription in this branch must have been spawned
-        SimultaneousAny,  // Any of the SpawnDescriptions in this branch must have been spawned
+        SimultaneousAll,  // Spawn all the things at once and wait for all of them to end before spawning the next set
         Sequential  // These are to be spawned sequentially
     }
 
@@ -69,10 +99,28 @@ namespace SpawnManager {
     public class SpawnDescription {
 
         public GameObject enemy;
-        public int number;
+        public int bursts = 1;
         public float timeBetweenBursts = 1;
         public int burstsOf = 1;
-        public float timeBetweenSpawns = 0.1f;
+        public float timeBetweenSpawns = 0.5f;
+
+        public IEnumerator DoSpawnAt(Vector3 position, EnemySpawnManager parent) {
+            for (int i = 0; i < bursts; i++) {
+                for (int j = 0; j < burstsOf; j++) {
+                    MonoBehaviour.Instantiate(enemy);
+                    enemy.transform.position = position;
+                    if (parent != null) {
+                        parent.OnCoroutineComplete();
+                    }
+                    yield return new WaitForSeconds(timeBetweenSpawns);
+                }
+                yield return new WaitForSeconds(timeBetweenBursts);
+            }
+        }
+
+        public IEnumerator DoSpawnAt(Vector3 position) {
+            return DoSpawnAt(position, null);
+        }
 
     }
 }
