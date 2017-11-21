39ff8d99f5abf6943f8018fd0fb2e6cc02230d23
plenglin <maximupwnguin@gmail.com>
2017-11-21 11:08:40 -0800

    day cycle

--- Assets/Scripts/DayCycle.cs
+++ Assets/Scripts/DayCycle.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class DayCycle : MonoBehaviour {
 
+    public GameObject sun;
+    public List<Light> lamps;
+    public List<SolarControl> solars;
+
+    public float dayLength = 0;
+    public float time = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +18,15 @@ public class DayCycle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        time += Time.deltaTime / dayLength;
+        bool isDay = time < 0.5;
+        foreach (SolarControl s in solars) {
+            s.enabled = isDay;
+        }
+        foreach (Light l in lamps) {
+            l.enabled = !isDay;
+        }
+        Vector3 rot = sun.transform.rotation.eulerAngles;
+        sun.transform.rotation.eulerAngles.Set(rot.x, time * 360, rot.y);
 	}
 }
