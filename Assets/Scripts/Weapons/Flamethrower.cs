b0cc428a5ae90b707cc5aaf27cf34d797d006b09
plenglin <maximumpwnguin@gmail.com>
2017-12-26 20:43:48 -0800

    Fix flamethrower issues

--- Assets/Scripts/Weapons/Flamethrower.cs
+++ Assets/Scripts/Weapons/Flamethrower.cs
@@ -20,24 +20,28 @@ public class Flamethrower : MonoBehaviour {
     private GameObject player;
     private Vector3 lastPlayerPos;
     private SmoothedAverage sizeBuffer;
+    private CapsuleCollider areaOfEffect;
 
     private void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
         sizeBuffer = new SmoothedAverage(20, (maxRange + minRange) / 2);
         lastPlayerPos = player.transform.position;
+        areaOfEffect = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update() {
         running = Input.GetButton("Fire1");
         if (running) {
+
             Vector3 playerPos = player.transform.position;
             Vector3 displacement = playerPos - lastPlayerPos;
             float projection = Vector3.Dot(displacement.normalized, player.transform.forward);
             lastPlayerPos = playerPos;
             float length = Mathf.Lerp(minRange, maxRange, (projection + 1) / 2);
             sizeBuffer.push(length);
-            GetComponent<CapsuleCollider>().height = sizeBuffer.avg;
+
+            UpdateCollider(sizeBuffer.avg);
 
             partycools.Play();
             if (nextDamageTick <= Time.time) {
@@ -48,6 +52,11 @@ public class Flamethrower : MonoBehaviour {
                 }
             }
         }
+    }
+
+    private void UpdateCollider(float size) {
+        areaOfEffect.height = size;
+        areaOfEffect.center = new Vector3(0, 0, size / 2 + 1);
     }
 
     private void OnTriggerEnter(Collider other) {
