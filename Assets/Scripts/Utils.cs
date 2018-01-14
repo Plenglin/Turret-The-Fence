f1bdf72ea1f891afa1a6654138ee1c5bb79c8538
plenglin <maximumpwnguin@gmail.com>
2018-01-14 13:44:16 -0800

    sdflak

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -43,4 +44,47 @@ namespace Utils {
         }
 
     }
+
+    [System.Serializable]
+    public class WeightedLootTable {
+
+        public List<LootTableEntry> table;
+
+        private int sum;
+        private List<LootTableEntry> cumulativeTable;
+
+        public void GenerateCumulativeTable() {
+            sum = 0;
+            foreach (LootTableEntry entry in table) {
+                sum += entry.weight;
+                cumulativeTable.Add(new LootTableEntry(entry.loot, sum));
+            }
+        }
+
+        public GameObject Get() {
+            int result = UnityEngine.Random.Range(0, sum);
+            foreach (LootTableEntry entry in table) {
+                if (entry.weight >= result) {
+                    return entry.loot;
+                }
+            }
+            throw new IndexOutOfRangeException();
+        }
+
+    }
+
+    [System.Serializable]
+    public class LootTableEntry {
+
+        public GameObject loot;
+        public int weight = 1;
+
+        public LootTableEntry() {}
+
+        public LootTableEntry(GameObject loot, int weight) {
+            this.loot = loot;
+            this.weight = weight;
+        }
+    }
+    
 }
