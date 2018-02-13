36eebbd6d7659e9e9187207e40d4ed28ba4db5e6
plenglin <maximupwnguin@gmail.com>
2018-02-13 13:03:50 -0800

    E X T E N D A B L E mode

--- Assets/Scripts/Player/WeaponSwitcher.cs
+++ Assets/Scripts/Player/WeaponSwitcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TurretTheFence.Utils;
 using UnityEngine;
@@ -7,40 +8,89 @@ namespace TurretTheFence.Player {
 
     public class WeaponSwitcher : MonoBehaviour {
 
-        public List<GameObject> guns = new List<GameObject>();
-        private GameObject current;
-        private int index = 0;
+        public WeaponMode weaponMode = new WeaponMode();
+        private Mode[] modeCycle;
+        private int cycleIndex = 0;
+        private GameObject currentWeapon;
+
+        private void Awake() {
+            modeCycle = new Mode[] { weaponMode };
+        }
 
         void Start() {
-            guns.ForEach(go => go.SetActive(false));
-            if (guns.Count > 0) SwitchTo(0);
+            foreach (Mode m in modeCycle) {
+                m.OnStart();
+            }
         }
 
-        public void AddWeapon(GameObject weapon) {
-            guns.Add(weapon);
-        }
-
-        public void SwitchTo(int index) {
-            guns[this.index].SetActive(false);
-            guns[index].SetActive(true);
-            this.index = index;
+        public void CycleWeaponMode() {
+            cycleIndex = (cycleIndex + 1) % modeCycle.Length;
         }
 
         void Update() {
             float scroll = Input.GetAxis("Mouse ScrollWheel");
-            int count = guns.Count;
+            Mode current = modeCycle[cycleIndex];
             if (scroll > 0) {
-                SwitchTo((index + 1 + count) % count);
+                current.OnScrollIndex(-1);
             } else if (scroll < 0) {
-                SwitchTo((index + 1 + count) % count);
+                current.OnScrollIndex(1);
             }
             foreach (KeyValuePair<KeyCode, int> p in Constants.keyToNumber) {
-                if (Input.GetKeyDown(p.Key) && p.Value <= count) {
-                    SwitchTo(p.Value - 1);
+                if (Input.GetKeyDown(p.Key)) {
+                    current.OnSwitchIndex(p.Value - 1);
                     break;
                 }
+            }
+            if (Input.GetKeyDown(KeyCode.Tab)) {
+                CycleWeaponMode();
             }
         }
 
     }
+
+    interface Mode {
+        void OnStart();
+        void OnEnable();
+        void OnDisable();
+        void OnSwitchIndex(int index);
+        void OnScrollIndex(int direction);
+    }
+
+    [System.Serializable]
+    public class WeaponMode : Mode {
+
+        public List<GameObject> guns = new List<GameObject>();
+        public int index = 0;
+
+        public void OnStart() {
+            OnDisable();
+            OnEnable();
+        }
+
+        public void OnEnable() {
+            SwitchTo(index);
+        }
+
+        public void OnDisable() {
+            guns.ForEach(go => go.SetActive(false));
+        }
+
+        public void OnSwitchIndex(int index) {
+            if (index < guns.Capacity) {
+                SwitchTo(index);
+            }
+        }
+
+        private void SwitchTo(int index) {
+            guns[this.index].SetActive(false);
+            this.index = index;
+            guns[this.index].SetActive(true);
+        }
+        
+        public void OnScrollIndex(int direction) {
+            int count = guns.Capacity;
+            SwitchTo((index + direction + count) % count);
+        }
+    }
+
 }
