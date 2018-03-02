a791dca59157c0fb5ccd44925db14aec6d5cddb5
plenglin <maximupwnguin@gmail.com>
2018-03-02 12:44:43 -0800

    i finally got these scriptableobjects to work

--- Assets/Scripts/UI/WeaponBuyListener.cs
+++ Assets/Scripts/UI/WeaponBuyListener.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using CompleteProject;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 
 namespace TurretTheFence.UI {
     public class WeaponBuyListener : BuyListener {
+
+        public GameObject weapon;
+
         public override void OnBuy() {
-            throw new NotImplementedException();
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            PlayerWeapons weaponmanager = player.GetComponent<PlayerWeapons>();
+            Instantiate(weapon, player.transform);
+            weaponmanager.AddWeapon(weapon);
+        }
+
+        [MenuItem("Assets/Create/WeaponBuyListener")]
+        public static void CreateAsset() {
+            ScriptableObjectUtility.CreateAsset<WeaponBuyListener>();
         }
     }
 }
