14e956f072514dce462ad372ec66b71eb4577e70
plenglin <maximupwnguin@gmail.com>
2018-03-05 13:19:18 -0800

    b to open shop

---
+++ Assets/Scripts/UI/ShopOpener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TurretTheFence.UI {
+    public class ShopOpener : MonoBehaviour {
+
+        private GameObject shop;
+
+        private void Awake() {
+            shop = GameObject.FindWithTag("Shop");
+        }
+
+        private void Start() {
+            shop.SetActive(false);
+        }
+
+        void Update() {
+            if (Input.GetKeyDown(KeyCode.B)) {
+                Debug.Log("b");
+                shop.SetActive(!shop.activeSelf);
+            }
+        }
+    }
+}
